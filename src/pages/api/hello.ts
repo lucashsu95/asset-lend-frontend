1cf717849ad2cc6d5170c4bee5e343998521ac3d
lucashsu95 <lucashsu95@gmail.com>
2024-11-29 14:10:36 +0800

    mysql-err

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,10 +1,32 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import mysql from "mysql2/promise";
 
 type Data = {
-	name: string
-}
+  message: string;
+};
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-	res.status(200).json({ name: 'John Doe' })
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  try {
+    const connection = await mysql.createConnection({
+      host: "borrow-db",
+      user: "root",
+      password: "1234",
+      database: "borrow-db",
+    });
+
+    const [rows] = await connection.execute<mysql.RowDataPacket[]>("SELECT * FROM users");
+    await connection.end();
+
+    if (rows.length === 0) {
+      res.status(404).json({ message: "No data found" });
+    } else {
+      res.status(200).json({ message: (rows[0] as { message: string }).message });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
